b284f35622a2936927b1e09e8e290bc45e3641f3
but9t <aw41033@gmail.com>
2024-03-12 21:22:55 +0700

    update client

---
+++ client.js
@@ -0,0 +1,11 @@
+function submitForm() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var message = document.getElementById('message').value;
+
+    console.log ('Name' + name);
+    console.log ('Email' + email);
+    console.log ('Phone' + phone);
+    console.log ('Message' + message);
+}
